e4471874c706e2e6bd32a0dd82c9bb4f02176648
Steven Chen <stevenchen0330@gmail.com>
2013-12-30 23:25:29 -0800

    finished

--- jscript.js
+++ jscript.js
@@ -1,5 +1,14 @@
-$(document).ready(function(){
-	$(".item").hover(function(){
-		$("this").slideUp();
-	});
+$(document).ready(function($) {
+
+    $(".clickable").click(function(event) {
+        event.preventDefault(); 
+
+        var defaultAnchorOffset = 0;
+        
+        var $anchor = $('#' + this.hash.substring(1));
+
+        $('html,body').animate({ 
+            scrollTop: $anchor.offset().top
+        }, 500);        
+    });
 });
