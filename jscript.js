37ae1af8dcac758ec4e642dc7d183bb3b44c7983
Steven Chen <stevenchen0330@gmail.com>
2014-01-05 22:55:33 -0800

    resume

--- jscript.js
+++ jscript.js
@@ -1,23 +1,21 @@
 $(document).ready(function($) {
 
-    $(".clickable").click(function(event) {
-        event.preventDefault(); 
-        
-        var $anchor = $('#' + this.hash.substring(1));
-
-        $('html,body').animate({ 
-            scrollTop: $anchor.offset().top
-        }, 500);        
-    });
+	$(".clickable").click(function(event) {
+		event.preventDefault(); 
+		var $anchor = $('#' + this.hash.substring(1));
+		$('html,body').animate({ 
+			scrollTop: $anchor.offset().top
+		}, 500);        
+	});
     
-    $("#top-button").click(function(event) {
-        event.preventDefault(); 
+	$("#top-button").click(function(event) {
+		event.preventDefault(); 
         
-        var $anchor = $('#' + this.hash.substring(1));
+    var $anchor = $('#' + this.hash.substring(1));
 
-        $('html,body').animate({ 
-            scrollTop: $anchor.offset().top
-        }, 500);        
-    });
+    $('html,body').animate({ 
+			scrollTop: $anchor.offset().top
+		}, 500);        
+	});
 
 });
